3ee08fced5504813970dd7c8e0881649c4e83ad4
GUPTA Aman <aman.gupta@hexagon.com>
2020-05-04 13:23:39 +0530

    Updates

---
+++ precache-manifest.74a01937d9f96c25e92711cb4097ba3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703971dd980dbd7c9073d2f7b87a5e84",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fd9ca2fb0fa095947ccf",
+    "url": "/monsters-rolodex/static/css/main.6f52610c.chunk.css"
+  },
+  {
+    "revision": "32a028debccbf86addd4",
+    "url": "/monsters-rolodex/static/js/2.b6dc672a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b6dc672a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd9ca2fb0fa095947ccf",
+    "url": "/monsters-rolodex/static/js/main.e21d7c9d.chunk.js"
+  },
+  {
+    "revision": "171b5ab0fa41b0bf4384",
+    "url": "/monsters-rolodex/static/js/runtime-main.68cdaae8.js"
+  }
+]);
